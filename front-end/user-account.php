6db1ba3eb1f8f9aa0be55e876361bb956070fbb4
Phuong <PhuongNHGCS200104@fpt.edu.vn>
2021-10-03 01:28:44 +0700

    update

--- front-end/user-account.php
+++ front-end/user-account.php
@@ -7,8 +7,37 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/user-account.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300&display=swap" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
+  
   <title></title>
+</head>
+
+<body>
+   <div class = "container"> 
+        <div class = "user-info">
+            <div class = "user-info-detail">
+                <div class = "user-image">
+                </div>
+                <div class = "info">
+                    <h1 id = "user-name">User Name</h2>
+                    <p id = "user-id">User id: 01</p>
+                    <p>Giới tính: </p>
+                </div>
+            </div>
+        </div>
+        <div class = "side-bar">
+            <ul>
+                <li><a href=""><i class="fas fa-home"></i></a></li>
+                <li><a href="">Play List</a></li>
+                <li><a href="">Thông tin</a></li>
+                <li><a href="">Bạn bè</a></li>
+            </ul>
+        </div>
+    </div>
 
 </body>
 </html>
-<?php

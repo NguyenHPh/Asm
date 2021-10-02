6959dab3e2e9c4949a8010ed9616a9c4a7306b74
Phuong <PhuongNHGCS200104@fpt.edu.vn>
2021-10-02 22:22:51 +0700

    update

--- front-end/product_detail.php
+++ front-end/product_detail.php
@@ -7,7 +7,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Music Player | CodingNepal</title>
+  <title></title>
   <link rel="stylesheet" href="../css/music-player.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
---
+++ front-end/user-account.php
@@ -0,0 +1,14 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - youtube.com/codingnepal -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+
+</body>
+</html>
+<?php
